4328fd8eac7342ef71de8649b937d3ad21cc08c8
Aarav Singh <aarav123@gmail.com>
2026-06-01 10:38:09 +0530

    Day 1 work

---
+++ Day01/Day02.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n;
+    cout<<"enter the value of n";
+    cin>>n;
+    for (int i = 1; i <=10; i++)
+    {
+        cout<<n*i<<"\n";
+    }
+    return 0;
+}
